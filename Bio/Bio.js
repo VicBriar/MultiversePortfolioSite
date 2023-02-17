b2d1cac9fc14a5ca8512b88f24baa75e722a28b4
Victoria McCrady <spartanginger@gmail.com>
2023-02-16 23:18:58 -0800

    new assets and updates from last commit

---
+++ Bio/Bio.js
@@ -0,0 +1,12 @@
+// function openNav() {
+//     document.getElementById("mainNav").style.width = "100%";
+//     document.getElementById("mainNav").style.height = "100%";
+// }
+// function closeNav() {
+//     document.getElementById("mainNav").style.width = "0%";
+// }
+
+
+function closeNav() {
+    document.getElementById("navBox").style.width = "50px";
+}
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+// function openNav() {
+//     document.getElementById("mainNav").style.width = "100%";
+//     document.getElementById("mainNav").style.height = "100%";
+// }
+// function closeNav() {
+//     document.getElementById("mainNav").style.width = "0%";
+// }
+
 function openNav() {
     document.getElementById("mainNav").style.width = "100%";
+    document.getElementById("mainNav").style.height = "100%";
+    document.getElementById("navBox").style.background = "rgba(0,0,0,.5)";
+    document.getElementById("navBox").style.width = "100vw";
+    document.getElementById("svg-button-box").style.background = "rgba(0,0,0,0)";
+    document.getElementById("svg-button-box").style.border = "none";
 }
 function closeNav() {
     document.getElementById("mainNav").style.width = "0%";
+    document.getElementById("navBox").style.width = "250px";
+    document.getElementById("navBox").style.background = "rgba(0,0,0,0)";
+    document.getElementById("svg-button-box").style.background = "rgba(0,0,0,0.5)";
+    document.getElementById("svg-button-box").style.border = "solid .1rem #ffffff"
 }
